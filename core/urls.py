270d8072ce20839538c2f7a1b29608254a8ce7a9
shafiqalandari <shafifazli73@gmail.com>
2026-04-08 12:29:59 +0430

    some changes

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('', include('finance.urls')),
+    path('customers/', include('customers.urls')),
 ]
---
+++ customers/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'customers'
+
+urlpatterns = [
+    path('', views.customer_list, name='customer_list'),
+    path('create/', views.customer_create, name='customer_create'),
+    path('<int:pk>/', views.customer_detail, name='customer_detail'),
+    path('<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+    path('<int:pk>/toggle-status/', views.toggle_customer_status, name='toggle_status'),
+    path('<int:customer_pk>/add-interaction/', views.add_interaction, name='add_interaction'),
+]
